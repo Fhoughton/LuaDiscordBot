082601f42c356246e9996a5bc2122cd6d8cabdf3
Some Dude <fdphoughton@protonmail.com>
2024-07-08 09:03:01 +0100

    Add processing of stack operations (add and multiply)

--- game_logic.lua
+++ game_logic.lua
@@ -45,6 +45,7 @@ function processInstruction(state, input)
 
     local input_asnum = tonumber(input)
     if input_asnum then
+        -- If it's a number and it's valid for that level push it to the stack
         state_level = state["level"]
 
         level_data = levels[state_level]
@@ -53,7 +54,21 @@ function processInstruction(state, input)
             table.insert(input_stack, input_asnum)
         end
     else
+        -- If it's a symbol and it's valid for that level try to process it on the stack, all take two arguments
+        local top_stack_item = input_stack[1]
+        local second_stack_item = input_stack[2]
 
+        if top_stack_item and second_stack_item then
+            -- Process symbol, push result to front
+            if input == "+" then table.insert(input_stack, 1, top_stack_item + second_stack_item)
+            elseif input == "*" then table.insert(input_stack, 1, top_stack_item * second_stack_item)
+            else return input_stack -- No operation done, just exit so we can be generic after
+            end
+
+            -- Remove two items below top to remove old data (operands of the symbol)
+            table.remove(input_stack, 2) -- [A+B,A,B] --> [A+B,B]
+            table.remove(input_stack, 2) -- [A+B,B] --> [A+B]
+        end
     end
 
     return input_stack
